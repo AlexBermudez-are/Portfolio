fb090245be76edee08e24cae030b31c8d118c64f
Edwin Alexis <edbwinn@gmail.com>
2022-01-29 19:06:08 -0300

    scroll home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BodyHome from '../Home/BodyHome'
 import FooterHome from '../Home/FooterHome'
 import HeaderHome from '../Home/HeaderHome'
 
 const Home = () => {
     const [state, setstate] = useState(false)
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
     return (
         <div>
             <HeaderHome state={state} setstate={setstate} />
